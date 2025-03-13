1142be6ce0ac54cf511bf7167cb243c32dd298d9
Arthur Villar <arthur@iMac.local>
2025-03-13 14:01:27 -0400

    Quick update

--- resources/views/ebook/hero.blade.php
+++ resources/views/ebook/hero.blade.php
@@ -4,7 +4,7 @@
             <div class="col-lg-6 col-md-6 col-12 d-flex justify-content-center flex-column">
                 <div class="px-4 sm-center" style="margin: 6rem 0;">
                     <p class="small text-red">60% DISCOUNT ON EARLY PURCHASE</p>
-                    <h1>Choosing a High School in NYC</h1>
+                    <h1 style="line-height: 1.1">Choosing a High School in NYC</h1>
                     <p>Your ultimate guide to finding the best high school in the five boroughs of New York City.</p>
                     @include('ebook.buybtn')
                     <p class="small opacity-6">*eBook includes PDF & ePub versions</p>
