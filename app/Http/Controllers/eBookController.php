89056968c2618908ac84424871c8a7430996ad78
Arthur Villar <arthur@iMac.local>
2025-04-03 23:31:33 -0400

    Quick update

--- app/Http/Controllers/eBookController.php
+++ app/Http/Controllers/eBookController.php
@@ -8,12 +8,16 @@ class eBookController extends Controller
 {
     public function index()
     {
-
         return view('ebook.index');
     }
 
-    public function purchase()
+    public function purchase(Request $request)
     {
+        $referer = $request->headers->get('referer');
+
+        if (!$referer || !str_contains(parse_url($referer, PHP_URL_HOST), 'stripe.com'))
+            abort(403);
+
         session(['ebook_purchased' => true]);
 
         return redirect(route('ebook.success'));
